8f98a854fc79f1c649ff6183c7f1b4aa99580fc7
Hugo O. Matus <hugoomarmatus@gmail.com>
2021-05-16 22:56:33 +0000

    install script

--- install_terraform.sh
+++ install_terraform.sh
@@ -15,6 +15,7 @@
     eksctl version
 }
 
+## Install Hashicorp terraform
 {
     curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
     sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
