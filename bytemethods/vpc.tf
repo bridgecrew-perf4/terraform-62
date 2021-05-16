6941bd195ed308e31d186dfc5453ee620e9b7dc3
Hugo Matus <hugoomarmatus@gmail.com>
2021-05-16 11:09:51 -0700

    vpc

---
+++ bytemethods/vpc.tf
@@ -0,0 +1,12 @@
+resource “aws_vpc” “prod-vpc” {
+    cidr_block = “10.0.0.0/16”
+    enable_dns_support = “true”
+    enable_dns_hostnames = “true”
+    enable_classiclink = “false”
+    instance_tenancy = “default”    
+    
+    tags {
+        Name = “theory-k8s-research”
+	Environment = "research"
+    }
+}
