4f00cbb8bb1066d85243379449312c2ad6d0ba9d
Hugo O. Matus <hugoomarmatus@gmail.com>
2021-05-16 22:47:55 +0000

    install script

--- install_terraform.sh
+++ install_terraform.sh
@@ -1,5 +1,22 @@
+# Installation
+
+## Install aws-cli
 {
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add - ;
-sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main" ;
-sudo apt install terraform;
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install
+    aws --version
+}
+
+## Install aws ekstl
+{
+    curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+    sudo mv /tmp/eksctl /usr/local/bin
+    eksctl version
+}
+
+{
+    curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+    sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+    sudo apt install terraform
 }
