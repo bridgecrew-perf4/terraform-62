576fda861cdd8eda345ec132ba97606ac233b3bf
Hugo Matus <hugomatus@me.com>
2021-05-16 21:25:13 -0700

    vpc + subnets - public, k8s and DB

--- bytemethods/vpc.tf
+++ bytemethods/vpc.tf
@@ -1,3 +1,4 @@
+#vpc 
 resource "aws_vpc" "research_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = "true"
@@ -10,24 +11,78 @@ resource "aws_vpc" "research_vpc" {
   }
 }
 
-resource "aws_subnet" "research_subnet_public_1" {
-    vpc_id = aws_vpc.research-vpc.id
-    cidr_block = "10.0.20.0/22"
+#public subnet A
+resource "aws_subnet" "research_subnet_public_A" {
+    vpc_id = aws_vpc.research_vpc.id
+    cidr_block = "10.0.0.0/22"
     map_public_ip_on_launch = "true"
     availability_zone = var.AWS_ZONE_A
     tags = {
-        Name = "research_subnet_public_1"
+        Name = "research-subnet-public-A"
         Environment = var.ENVIRONMENT
     }
 }
 
-resource "aws_subnet" "research_subnet_public_2" {
-    vpc_id = aws_vpc.research-vpc.id
+#public subnmet B
+resource "aws_subnet" "research_subnet_public_B" {
+    vpc_id = aws_vpc.research_vpc.id
     cidr_block = "10.0.4.0/22"
     map_public_ip_on_launch = "true"
     availability_zone = var.AWS_ZONE_B
     tags = {
-        Name = "research_subnet_public_2"
+        Name = "research-subnet-public-B"
+        Environment = var.ENVIRONMENT
+    }
+}
+
+
+#k8 subnet A
+resource "aws_subnet" "research_subnet_k8s_A" {
+    vpc_id = aws_vpc.research_vpc.id
+    cidr_block = "10.0.20.0/22"
+    map_public_ip_on_launch = "false"
+    availability_zone = var.AWS_ZONE_A
+    tags = {
+        Name = "research-subnet-k8s-A"
+        Environment = var.ENVIRONMENT
+    }
+}
+
+
+#k8 subnet B
+resource "aws_subnet" "research_subnet_k8s_B" {
+    vpc_id = aws_vpc.research_vpc.id
+    cidr_block = "10.0.24.0/22"
+    map_public_ip_on_launch = "false"
+    availability_zone = var.AWS_ZONE_B
+    tags = {
+        Name = "research-subnet-k8s-B"
+        Environment = var.ENVIRONMENT
+    }
+}
+
+
+#DB subnet A
+resource "aws_subnet" "research_subnet_db_A" {
+    vpc_id = aws_vpc.research_vpc.id
+    cidr_block = "10.0.48.0/20"
+    map_public_ip_on_launch = "false"
+    availability_zone = var.AWS_ZONE_A
+    tags = {
+        Name = "research-subnet-db-A"
+        Environment = var.ENVIRONMENT
+    }
+}
+
+
+#DB subnet B
+resource "aws_subnet" "research_subnet_db_B" {
+    vpc_id = aws_vpc.research_vpc.id
+    cidr_block = "10.0.64.0/20"
+    map_public_ip_on_launch = "false"
+    availability_zone = var.AWS_ZONE_B
+    tags = {
+        Name = "research-subnet-db-B"
         Environment = var.ENVIRONMENT
     }
 }
