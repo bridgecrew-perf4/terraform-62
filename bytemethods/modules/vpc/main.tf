c6b12acfadf5b53549c0542006c56692d414042d
Hugo Matus <hugomatus@me.com>
2021-05-18 09:15:15 -0700

    modules

--- bytemethods/modules/vpc/main.tf
+++ bytemethods/modules/vpc/main.tf
@@ -1,6 +1,6 @@
 # VPC 
 
-resource "aws_vpc" "research_vpc" {
+resource "aws_vpc" "rt_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = "true"
   enable_dns_hostnames = "true"
@@ -14,26 +14,26 @@ resource "aws_vpc" "research_vpc" {
 
 ## Public Subnet A
 
-resource "aws_subnet" "research_subnet_public_A" {
-  vpc_id                  = aws_vpc.research_vpc.id
+resource "aws_subnet" "rt_subnet_public_A" {
+  vpc_id                  = aws_vpc.rt_vpc.id
   cidr_block              = "10.0.0.0/22"
   map_public_ip_on_launch = "true"
   availability_zone       = var.AWS_ZONE_A
   tags = {
-    Name        = "research-subnet-public-A"
+    Name        = "{var.ENVIRONMENT}-subnet-public-A"
     Environment = var.ENVIRONMENT
   }
 }
 
 ## public Subnet B
 
-resource "aws_subnet" "research_subnet_public_B" {
-  vpc_id                  = aws_vpc.research_vpc.id
+resource "aws_subnet" "rt_subnet_public_B" {
+  vpc_id                  = aws_vpc.rt_vpc.id
   cidr_block              = "10.0.4.0/22"
   map_public_ip_on_launch = "true"
   availability_zone       = var.AWS_ZONE_B
   tags = {
-    Name        = "research-subnet-public-B"
+    Name        = "{var.ENVIRONMENT}-subnet-public-B"
     Environment = var.ENVIRONMENT
   }
 }
@@ -41,26 +41,26 @@ resource "aws_subnet" "research_subnet_public_B" {
 
 ## K8 Subnet A 
 
-resource "aws_subnet" "research_subnet_k8s_A" {
-  vpc_id                  = aws_vpc.research_vpc.id
+resource "aws_subnet" "rt_subnet_k8s_A" {
+  vpc_id                  = aws_vpc.rt_vpc.id
   cidr_block              = "10.0.20.0/22"
   map_public_ip_on_launch = "false"
   availability_zone       = var.AWS_ZONE_A
   tags = {
-    Name        = "research-subnet-k8s-A"
+    Name        = "{var.ENVIRONMENT}-subnet-k8s-A"
     Environment = var.ENVIRONMENT
   }
 }
 
 
 ## K8 Subnet B
-resource "aws_subnet" "research_subnet_k8s_B" {
-  vpc_id                  = aws_vpc.research_vpc.id
+resource "aws_subnet" "rt_subnet_k8s_B" {
+  vpc_id                  = aws_vpc.rt_vpc.id
   cidr_block              = "10.0.24.0/22"
   map_public_ip_on_launch = "false"
   availability_zone       = var.AWS_ZONE_B
   tags = {
-    Name        = "research-subnet-k8s-B"
+    Name        = "{var.ENVIRONMENT}-subnet-k8s-B"
     Environment = var.ENVIRONMENT
   }
 }
@@ -68,13 +68,13 @@ resource "aws_subnet" "research_subnet_k8s_B" {
 
 ## DB subnet A
 
-resource "aws_subnet" "research_subnet_db_A" {
-  vpc_id                  = aws_vpc.research_vpc.id
+resource "aws_subnet" "rt_subnet_db_A" {
+  vpc_id                  = aws_vpc.rt_vpc.id
   cidr_block              = "10.0.48.0/20"
   map_public_ip_on_launch = "false"
   availability_zone       = var.AWS_ZONE_A
   tags = {
-    Name        = "research-subnet-db-A"
+    Name        = "{var.ENVIRONMENT}-subnet-db-A"
     Environment = var.ENVIRONMENT
   }
 }
@@ -82,34 +82,34 @@ resource "aws_subnet" "research_subnet_db_A" {
 
 ## DB subnet B
 
-resource "aws_subnet" "research_subnet_db_B" {
-  vpc_id                  = aws_vpc.research_vpc.id
+resource "aws_subnet" "rt_subnet_db_B" {
+  vpc_id                  = aws_vpc.rt_vpc.id
   cidr_block              = "10.0.64.0/20"
   map_public_ip_on_launch = "false"
   availability_zone       = var.AWS_ZONE_B
   tags = {
-    Name        = "research-subnet-db-B"
+    Name        = "{var.ENVIRONMENT}-subnet-db-B"
     Environment = var.ENVIRONMENT
   }
 }
 
 
 ## Bastion Subnet A
-resource "aws_subnet" "research_subnet_bastion_A" {
-  vpc_id                  = aws_vpc.research_vpc.id
+resource "aws_subnet" "rt_subnet_bastion_A" {
+  vpc_id                  = aws_vpc.rt_vpc.id
   cidr_block              = "10.0.12.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = var.AWS_ZONE_A
   tags = {
-    Name        = "research-subnet-bastion-A"
+    Name        = "{var.ENVIRONMENT}-subnet-bastion-A"
     Environment = var.ENVIRONMENT
   }
 }
 
 ## IGW
 
-resource "aws_internet_gateway" "research_igw" {
-  vpc_id = aws_vpc.research_vpc.id
+resource "aws_internet_gateway" "rt_igw" {
+  vpc_id = aws_vpc.rt_vpc.id
 
   tags = {
     Name        = "internet-gateway"
@@ -120,11 +120,11 @@ resource "aws_internet_gateway" "research_igw" {
 ## Route Table
 
 resource "aws_default_route_table" "route_table" {
-  default_route_table_id = aws_vpc.research_vpc.default_route_table_id
+  default_route_table_id = aws_vpc.rt_vpc.default_route_table_id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.research_igw.id
+    gateway_id = aws_internet_gateway.rt_igw.id
   }
 
   tags = {
@@ -134,29 +134,29 @@ resource "aws_default_route_table" "route_table" {
 }
 
 resource "aws_route_table_association" "k8s_A" {
-  subnet_id      = aws_subnet.research_subnet_k8s_A.id
+  subnet_id      = aws_subnet.rt_subnet_k8s_A.id
   route_table_id = aws_default_route_table.route_table.id
 }
 
 
 resource "aws_route_table_association" "k8s_B" {
-  subnet_id      = aws_subnet.research_subnet_k8s_B.id
+  subnet_id      = aws_subnet.rt_subnet_k8s_B.id
   route_table_id = aws_default_route_table.route_table.id
 }
 
 resource "aws_route_table_association" "DB_A" {
-  subnet_id      = aws_subnet.research_subnet_db_A.id
+  subnet_id      = aws_subnet.rt_subnet_db_A.id
   route_table_id = aws_default_route_table.route_table.id
 }
 
 resource "aws_route_table_association" "DB_B" {
-  subnet_id      = aws_subnet.research_subnet_db_B.id
+  subnet_id      = aws_subnet.rt_subnet_db_B.id
   route_table_id = aws_default_route_table.route_table.id
 }
 
 
 # Elastic IP for NAT GW
-resource "aws_eip" "research_nat_eip" {
+resource "aws_eip" "rt_nat_eip" {
 
   vpc = true
 
@@ -167,30 +167,30 @@ resource "aws_eip" "research_nat_eip" {
 
 
 # NAT GW
-resource "aws_nat_gateway" "research_nat_gw" {
+resource "aws_nat_gateway" "rt_nat_gw" {
 
-  allocation_id = aws_eip.research_nat_eip.id
+  allocation_id = aws_eip.rt_nat_eip.id
 
-  subnet_id = aws_subnet.research_subnet_k8s_A.id
+  subnet_id = aws_subnet.rt_subnet_k8s_A.id
 
   tags = {
-    Name = "research-nat-gw"
+    Name = "{var.ENVIRONMENT}-nat-gw"
   }
 
   depends_on = [
-    aws_eip.research_nat_eip
+    aws_eip.rt_nat_eip
   ]
 
 }
 
-resource "aws_route_table" "research_nat_gw_rt" {
+resource "aws_route_table" "rt_nat_gw_rt" {
 
 
-  vpc_id = aws_vpc.research_vpc.id
+  vpc_id = aws_vpc.rt_vpc.id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.research_nat_gw.id
+    nat_gateway_id = aws_nat_gateway.rt_nat_gw.id
   }
 
   tags = {
@@ -198,7 +198,7 @@ resource "aws_route_table" "research_nat_gw_rt" {
   }
 
   depends_on = [
-    aws_nat_gateway.research_nat_gw
+    aws_nat_gateway.rt_nat_gw
   ]
 
 }
