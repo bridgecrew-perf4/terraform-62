66df5c3d0a29eb3a75e5aa1b1be5ffccc5863ef2
Hugo Matus <hugomatus@me.com>
2021-05-23 13:46:34 -0700

    refactor to include backend for state mgmt

---
+++ realtheory.io/live/global/s3/main.tf
@@ -0,0 +1,34 @@
+# State Mgmt
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "realtheory.io-terraform-state"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
---
+++ realtheory.io/modules/state-mgmt/vars.tf
@@ -0,0 +1,6 @@
+variable "environment" {
+  type        = string
+  description = "The environment such as dev, staging, research ,prod, etc."
+  default     = "research"
+
+}
